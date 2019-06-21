0855f04a3f942e79bcae0d2725df54436f818d94
cord walsh <cordwalsh@MacBook-Pro-3.local>
2019-06-21 11:17:59 -0700

    add multipls vars and set up array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 function Pizza (size, toppings) {
-  this.size = size,
-  this.toppings = toppings,
+  this.size = size;
+  this.toppings = toppings;
 }
-
-
-
+var yourPizza;
+var selectedToppings=[];
 
 
 
@@ -13,5 +12,20 @@ function Pizza (size, toppings) {
 $(document).ready(function () {
   $('form#toppings-dropdown').submit(function(event){
     event.preventDefault();
-
-    var yourPizza = new Pizza
+      var selectedSize = $("#title").val();
+      var selectedCheese = $("#selectedCheese").val();
+      var selectedPepperoni = $("#selectedPepperoni").val();
+      var selectedSausage = $("#selectedSausage").val();
+      var selectedMushrooms = $("#selectedMushrooms").val();
+      var selectedOlives = $("#selectedOlives").val();
+      var selectedOnions = $("#selectedOnions").val();
+      var selectedPeppers = $("#selectedPeppers").val();
+      var selectedPineapple = $("#selectedPineapple").val();
+      var selectedAnchovy = $("#selectedAnchovy").val();
+      yourPizza = new Pizza (selectedSize, selectedToppings);
+      selectedToppings = [selectedCheese,selectedPepperoni,selectedSausage,selectedMushrooms,selectedOlives,selectedOnions,selectedPeppers,selectedPineapple,selectedAnchovy];
+      // var
+      console.log(selectedSize)
+      console.log(selectedToppings)
+});
+});
