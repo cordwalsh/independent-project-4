20eb2d42d4eafd3514e8ec02ec4e524f373b302c
cord walsh <cordwalsh@MacBook-Pro-3.local>
2019-06-21 11:43:44 -0700

    add funtion to check which boxes are checked

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,20 @@ $(document).ready(function () {
   $('form#toppings-dropdown').submit(function(event){
     event.preventDefault();
       var selectedSize = $("#title").val();
-      var selectedCheese = $("#selectedCheese").val();
-      var selectedPepperoni = $("#selectedPepperoni").val();
-      var selectedSausage = $("#selectedSausage").val();
-      var selectedMushrooms = $("#selectedMushrooms").val();
-      var selectedOlives = $("#selectedOlives").val();
-      var selectedOnions = $("#selectedOnions").val();
-      var selectedPeppers = $("#selectedPeppers").val();
-      var selectedPineapple = $("#selectedPineapple").val();
-      var selectedAnchovy = $("#selectedAnchovy").val();
+      var selectedCheese = parseInt($("#selectedCheese").val());
+      var selectedPepperoni = parseInt($("#selectedPepperoni").val());
+      var selectedSausage = parseInt($("#selectedSausage").val());
+      var selectedMushrooms = parseInt($("#selectedMushrooms").val());
+      var selectedOlives = parseInt($("#selectedOlives").val());
+      var selectedOnions = parseInt($("#selectedOnions").val());
+      var selectedPeppers = parseInt($("#selectedPeppers").val());
+      var selectedPineapple = parseInt($("#selectedPineapple").val());
+      var selectedAnchovy = parseInt($("#selectedAnchovy").val());
       yourPizza = new Pizza (selectedSize, selectedToppings);
-      selectedToppings = [selectedCheese,selectedPepperoni,selectedSausage,selectedMushrooms,selectedOlives,selectedOnions,selectedPeppers,selectedPineapple,selectedAnchovy];
+      $.each($("input[name='topping']:checked"), function(){
+        selectedToppings.push($(this).val());
+      });
+      // selectedToppings = [selectedCheese,selectedPepperoni,selectedSausage,selectedMushrooms,selectedOlives,selectedOnions,selectedPeppers,selectedPineapple,selectedAnchovy];
       // var
       console.log(selectedSize)
       console.log(selectedToppings)
